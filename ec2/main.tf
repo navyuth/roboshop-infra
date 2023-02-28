8f04e669a0aa2b1a77668a3c7c7f7efa13ac7bc9
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-28 18:50:34 +0530

    changes in main.tf files and removed variable sg_id

--- ec2/main.tf
+++ ec2/main.tf
@@ -7,7 +7,7 @@ data "aws_ami" "ami" {
 resource "aws_instance" "ec2" {
   ami = "ami-0a017d8ceb274537d"
   instance_type = var.instance_type
-  vpc_security_group_ids = [var.sg_id]
+  vpc_security_group_ids = [aws_security_group.sg.id]
   tags = {
     Name = var.component
   }
