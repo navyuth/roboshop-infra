ddef93e868197b695acf16f583b0f93155ac1f0f
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-28 19:51:49 +0530

    added password variable

--- ec2/main.tf
+++ ec2/main.tf
@@ -24,7 +24,7 @@ resource "null_resource" "provisioner" {
     inline = [
       "git clone https://github.com/navyuth/roboshop-shell",
       "cd roboshop-shell",
-      "sudo bash ${var.component}.sh"
+      "sudo bash ${var.component}.sh ${var.password}"
 
     ]
   }
@@ -68,3 +68,4 @@ variable "instance_type" {}
 variable "env" {
         default = "dev"
 }
+variable "password" {}
