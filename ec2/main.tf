74bd33e89fbf3021518d31134daff4b051cc8819
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-28 20:38:49 +0530

    added public_id in provisioner

--- ec2/main.tf
+++ ec2/main.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "ec2" {
 resource "null_resource" "provisioner" {
   provisioner "remote-exec" {
     connection {
-      host = aws_instance.ec2.private_ip
+      host = aws_instance.ec2.public_ip
       user = "centos"
       password = "DevOps321"
     }
