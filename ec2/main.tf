4c887ee6fa91e1439f594b2ab5fb83b8dbfc9320
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-03-09 01:00:28 +0530

    created policy, roles and Instance profile

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "ec2" {
   ami = data.aws_ami.ami.image_id
   instance_type = var.instance_type
+  iam_instance_profile = "${var.env}-${var.component}-role"
   vpc_security_group_ids = [aws_security_group.sg.id]
   tags = {
     Name = var.component
