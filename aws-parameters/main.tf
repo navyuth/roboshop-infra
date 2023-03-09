c46e5d1f02fddc61341e28051942355e32d72ea4
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-03-09 15:11:09 +0530

    added secrets

--- aws-parameters/main.tf
+++ aws-parameters/main.tf
@@ -6,7 +6,7 @@ resource "aws_ssm_parameter" "parameter" {
 }
 
 resource "aws_ssm_parameter" "secrets" {
-  count =length(var.secrets)
+  count = length(var.secrets)
   name  = var.parameters[count.index].name
   type  = var.parameters[count.index].type
   value = var.parameters[count.index].value
