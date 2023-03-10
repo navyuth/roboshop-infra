47af05f9b9216feab224e7b136c8e5b7ebff2681
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-03-10 11:16:21 +0530

    added vars directory for each component

--- aws-parameters/main.tf
+++ aws-parameters/main.tf
@@ -5,7 +5,7 @@ resource "aws_ssm_parameter" "parameter" {
   value = var.parameters[count.index].value
 }
 
-resource "aws_ssm_parameter" "secrets" {
+resource "aws_ssm_parameter" "secret" {
   count = length(var.secrets)
   name  = var.parameters[count.index].name
   type  = var.parameters[count.index].type
