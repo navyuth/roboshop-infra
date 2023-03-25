38cd7389a95eec5af1d5c84bc93c0f2ed6cc2c49
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-03-25 21:46:52 +0530

    updated rabbitmq

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ module "docdb" {
 
   backup_retention_period = each.value["backup_retention_period"]
   preferred_backup_window = each.value["preferred_backup_window"]
-  skip_final_snapshot = each.value["skip_final_snapshot "]
+  skip_final_snapshot = each.value["skip_final_snapshot"]
   no_of_instances = each.value["no_of_instances"]
   instance_class = each.value["instance_class"]
 }
