73b7b1123b0b44eb32d6ed90f092fbd0c70b23bc
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-03-25 12:35:53 +0530

    added peer connection and created variables default vpc and rouetable, auto_accept

--- main.tf
+++ main.tf
@@ -2,6 +2,9 @@ module "vpc" {
   source = "git::https://github.com/navyuth/tf-module-vpc.git"
   env = var.env
   tags = var.tags
+  default_vpc_id = var.default_vpc_id
+  default_route_table = var.default_route_table
+
   for_each = var.vpc
   vpc_cidr = each.value["vpc_cidr"]
   public_subnets = each.value["public_subnets"]
