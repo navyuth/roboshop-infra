77b2f6c7dd1faa43ce41398917eca885ab8b4b6f
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-03-25 20:42:31 +0530

    created rds

--- main.tf
+++ main.tf
@@ -28,9 +28,18 @@ module "docdb" {
   skip_final_snapshot = each.value["skip_final_snapshot "]
   no_of_instances = each.value["no_of_instances"]
   instance_class = each.value["instance_class"]
-
 }
 
-output "vpc" {
-  value = local.db_subnet_ids
+module "rds" {
+  source = "git::https://github.com/navyuth/tf-module-rds.git"
+  env    = var.env
+  tags   = var.tags
+
+  subnet_ids = local.db_subnet_ids
+
+  for_each       = var.rds
+  engine         = each.value["engine"]
+  engine_version = each.value["engine_version"]
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
 }
