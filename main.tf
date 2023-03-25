599cf132bbc1a4375378a300203c070fbe4cb575
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-03-25 19:19:29 +0530

    added locals.tf and tolist function

--- main.tf
+++ main.tf
@@ -19,10 +19,12 @@ module "docdb" {
 
   for_each = var.docdb
   engine   = each.value["engine"]
+  engine_version = each.value["engine_version"]
+
   backup_retention_period = each.value["backup_retention_period"]
   preferred_backup_window = each.value["preferred_backup_window"]
   skip_final_snapshot = each.value["skip_final_snapshot "]
-  engine_version = each.value["engine_version"]
+
   subnet_ids = local.db_subnet_ids
 }
 
