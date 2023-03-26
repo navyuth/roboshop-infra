cfb6bacacd00bcd525105018a818ff40a50bbce7
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-03-26 11:38:52 +0530

    created apps

--- main.tf
+++ main.tf
@@ -42,7 +42,6 @@ module "rds" {
   engine_version = each.value["engine_version"]
   backup_retention_period = each.value["backup_retention_period"]
   preferred_backup_window = each.value["preferred_backup_window"]
-  skip_final_snapshot = each.value["skip_final_snapshot"]
   no_of_instances = each.value["no_of_instances"]
   instance_class = each.value["instance_class"]
 }
