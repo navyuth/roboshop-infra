4d898a7b193c58af0ae5ed19047295525b54eabc
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-03-26 12:05:12 +0530

    created launch template and ASG

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -112,6 +112,10 @@ apps = {
   catalogue = {
     component = "catalogue"
     instance_type = "t3.micro"
+    desired_capacity   = 1
+    max_size           = 4
+    min_size           = 1
+    subnet_name = "app"
 
   }
 }
--- main.tf
+++ main.tf
@@ -91,4 +91,8 @@ module "apps" {
   for_each = var.apps
   component = each.value["component"]
   instance_type = each.value["instance_type"]
+  desired_capacity   = each.value["desired_capacity"]
+  max_size           = each.value["max_size"]
+  min_size           = each.value["min_size"]
+  subnets = lookup(local.subnet_ids, each.value["subnet_name"], null)
 }
