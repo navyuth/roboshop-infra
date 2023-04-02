6753420ff4424cc9e03b88c8727c787b18d81601
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-04-02 09:38:34 +0530

    created security group to allow app subnets

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ module "docdb" {
   skip_final_snapshot = each.value["skip_final_snapshot"]
   no_of_instances = each.value["no_of_instances"]
   instance_class = each.value["instance_class"]
-  #allow_subnets = lookup(local.subnet_cidr, each.value["allow_subnets"], null )
+  allow_subnets = lookup(local.subnet_cidr, each.value["allow_subnets"], null )
 }
 
 module "rds" {
