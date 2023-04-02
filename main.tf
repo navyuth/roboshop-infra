47d06c376d541c514f1a994136dc9304208f258e
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-04-02 08:21:35 +0530

    added allow_cidr & vpc_id for db components

--- main.tf
+++ main.tf
@@ -11,63 +11,72 @@ module "vpc" {
   private_subnets = each.value["private_subnets"]
 }
 
-#module "docdb" {
-#  source = "git::https://github.com/navyuth/tf-module-docdb.git"
-#  env    = var.env
-#  tags   = var.tags
-#
-#  subnet_ids = local.db_subnet_ids
-#
-#  for_each = var.docdb
-#  engine   = each.value["engine"]
-#  engine_version = each.value["engine_version"]
-#
-#  backup_retention_period = each.value["backup_retention_period"]
-#  preferred_backup_window = each.value["preferred_backup_window"]
-#  skip_final_snapshot = each.value["skip_final_snapshot"]
-#  no_of_instances = each.value["no_of_instances"]
-#  instance_class = each.value["instance_class"]
-#}
-#
-#module "rds" {
-#  source = "git::https://github.com/navyuth/tf-module-rds.git"
-#  env    = var.env
-#  tags   = var.tags
-#
-#  subnet_ids = local.db_subnet_ids
-#
-#  for_each       = var.rds
-#  engine         = each.value["engine"]
-#  engine_version = each.value["engine_version"]
-#
-#  backup_retention_period = each.value["backup_retention_period"]
-#  preferred_backup_window = each.value["preferred_backup_window"]
-#  no_of_instances = each.value["no_of_instances"]
-#  instance_class = each.value["instance_class"]
-#}
-#
-#module "elasticache" {
-#  source = "git::https://github.com/navyuth/tf-module-elasticache.git"
-#  env    = var.env
-#  tags   = var.tags
-#  subnet_ids = local.db_subnet_ids
-#
-#  for_each       = var.elasticache
-#  engine         = each.value["engine"]
-#  engine_version = each.value["engine_version"]
-#  num_cache_nodes = each.value["num_cache_nodes"]
-#  node_type = each.value["node_type"]
-#}
-#
-#module "rabbitmq" {
-#  source = "git::https://github.com/navyuth/tf-module-rabbitmq.git"
-#  env    = var.env
-#  tags   = var.tags
-#  subnet_ids = local.db_subnet_ids
-#
-#  for_each      = var.rabbitmq
-#  instance_type = each.value["instance_type"]
-#}
+module "docdb" {
+  source = "git::https://github.com/navyuth/tf-module-docdb.git"
+  env    = var.env
+  tags   = var.tags
+
+  subnet_ids = local.db_subnet_ids
+  vpc_id = module.vpc["main"].vpc_id
+
+
+  for_each = var.docdb
+  engine   = each.value["engine"]
+  engine_version = each.value["engine_version"]
+
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  skip_final_snapshot = each.value["skip_final_snapshot"]
+  no_of_instances = each.value["no_of_instances"]
+  instance_class = each.value["instance_class"]
+  allow_subnets = lookup(local.subnet_cidr, each.value["allow_subnets"], null )
+}
+
+module "rds" {
+  source = "git::https://github.com/navyuth/tf-module-rds.git"
+  env    = var.env
+  tags   = var.tags
+
+  subnet_ids = local.db_subnet_ids
+  vpc_id = module.vpc["main"].vpc_id
+
+  for_each       = var.rds
+  engine         = each.value["engine"]
+  engine_version = each.value["engine_version"]
+
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  no_of_instances = each.value["no_of_instances"]
+  instance_class = each.value["instance_class"]
+  allow_subnets = lookup(local.subnet_cidr, each.value["allow_subnets"], null )
+}
+
+module "elasticache" {
+  source = "git::https://github.com/navyuth/tf-module-elasticache.git"
+  env    = var.env
+  tags   = var.tags
+  subnet_ids = local.db_subnet_ids
+  vpc_id = module.vpc["main"].vpc_id
+
+  for_each       = var.elasticache
+  engine         = each.value["engine"]
+  engine_version = each.value["engine_version"]
+  num_cache_nodes = each.value["num_cache_nodes"]
+  node_type = each.value["node_type"]
+  allow_subnets = lookup(local.subnet_cidr, each.value["allow_subnets"], null )
+}
+
+module "rabbitmq" {
+  source = "git::https://github.com/navyuth/tf-module-rabbitmq.git"
+  env    = var.env
+  tags   = var.tags
+  subnet_ids = local.db_subnet_ids
+  vpc_id = module.vpc["main"].vpc_id
+
+  for_each      = var.rabbitmq
+  instance_type = each.value["instance_type"]
+  allow_subnets = lookup(local.subnet_cidr, each.value["allow_subnets"], null )
+}
 
 module "alb" {
   source = "git::https://github.com/navyuth/tf-module-alb.git"
