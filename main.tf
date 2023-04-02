71f975c6ec2c478e53fd1aed55a3955961858222
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-04-02 12:44:02 +0530

    updated rabbitmq for payment service

--- main.tf
+++ main.tf
@@ -70,12 +70,14 @@ module "rabbitmq" {
   source = "git::https://github.com/navyuth/tf-module-rabbitmq.git"
   env    = var.env
   tags   = var.tags
+  bastion_cidr = var.bastion_cidr
+  dns_domain = var.dns_domain
   subnet_ids = local.db_subnet_ids
-  #vpc_id = module.vpc["main"].vpc_id
+  vpc_id = module.vpc["main"].vpc_id
 
   for_each      = var.rabbitmq
   instance_type = each.value["instance_type"]
-  #allow_subnets = lookup(local.subnet_cidr, each.value["allow_subnets"], null )
+  allow_subnets = lookup(local.subnet_cidr, each.value["allow_subnets"], null )
 }
 
 module "alb" {
